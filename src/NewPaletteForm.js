52fc3fa077dff3d7412c6643ec128093be29ba07
David Arroyo <arroyoschneiderman@gmail.com>
2020-06-11 11:35:35 -0400

    Refactor new palette nav to own component

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -3,19 +3,17 @@ import {ChromePicker} from 'react-color';
 import Button from '@material-ui/core/Button'
 import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
 import DraggableColorList from './DraggableColorList.js';
+import PaletteFormNav from './PaletteFormNav.js'
 
 
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 import {arrayMove} from 'react-sortable-hoc';
 
@@ -88,8 +86,7 @@ class PersistentDrawerLeft extends React.Component {
         this.state= {
             currentColor: 'teal',
             newColorName: '',
-            colors: this.props.palettes[0].colors,
-            newPaletteName: ''
+            colors: this.props.palettes[0].colors
         }
         this.updateCurrentColor = this.updateCurrentColor.bind(this);
         this.addNewColor = this.addNewColor.bind(this);
@@ -112,19 +109,12 @@ class PersistentDrawerLeft extends React.Component {
             ({color}) => color !== this.state.currentColor
             )
     );
-    //
-    ValidatorForm.addValidationRule('isPaletteNameUnique', value =>
-    this.props.palettes.every(
-        ({paletteName}) => paletteName.toLowerCase() !== value.toLowerCase()
-        )
-);
     }
 
-    handleSubmit() {
-      let newName = this.state.newPaletteName;
+    handleSubmit(newPaletteName) {
       const newPalette = {
-        paletteName: newName,
-        id: newName.toLowerCase().replace(/ /g, '-'), 
+        paletteName: newPaletteName,
+        id: newPaletteName.toLowerCase().replace(/ /g, '-'), 
         colors: this.state.colors
       }
         this.props.savePalette(newPalette);
@@ -183,53 +173,21 @@ class PersistentDrawerLeft extends React.Component {
 
 
   render() {
-    const { classes, maxColors } = this.props;
+    const { classes, maxColors, palettes } = this.props;
     const { open, colors } = this.state;
     const paletteIsFull = colors.length >= maxColors;
 
 
     return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          color="inherit"
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Persistent drawer
-            </Typography>
+      <div className={classes.root}>  
+      <PaletteFormNav 
+      open={open} 
+      classes={classes} 
+      palettes={palettes} 
+      handleSubmit={this.handleSubmit}
+      handleDrawerOpen={this.handleDrawerOpen}
+      />
 
-            <ValidatorForm onSubmit={this.handleSubmit}>
-              <TextValidator 
-              label='Palette Name' 
-              name='newPaletteName'
-              value={this.state.newPaletteName}
-              onChange={this.handleChange}
-              validators={['required', 'isPaletteNameUnique']}
-              errorMessages={['All palettes need a name', 'Name already used']}
-              />
-              <Button
-              variant='contained'
-              color='secondary'
-              type='submit'
-              >
-              Save Palette
-              </Button>
-            </ValidatorForm>
-          </Toolbar>
-        </AppBar>
         <Drawer
           className={classes.drawer}
           variant="persistent"
@@ -239,7 +197,13 @@ class PersistentDrawerLeft extends React.Component {
             paper: classes.drawerPaper,
           }}
         >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
           <Divider />
+
 {/*  
 
  PORQUE DESABILITE EL NEWCOLORFORM, YA QUE QUE EN LA VERSION ANTERIOR NEWPALETTEFORM ERA UNA FUNCION, 
@@ -248,7 +212,7 @@ class PersistentDrawerLeft extends React.Component {
 */}
 {/* <NewColorForm /> // en vez de todo el div de abajo*/}
           <div>
-                <h1>HELLLOOOOO</h1>
+                <Typography variant='h4'>Design Your Palette</Typography>
                 <div>
                 <Button 
                 variant='contained' 
@@ -320,142 +284,3 @@ PersistentDrawerLeft.propTypes = {
 };
 
 export default withStyles(styles, { withTheme: true })(PersistentDrawerLeft);
-
-// import clsx from 'clsx';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Drawer from '@material-ui/core/Drawer';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Divider from '@material-ui/core/Divider';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-
-
-// const drawerWidth = 400;
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: 'flex',
-//   },
-//   appBar: {
-//     transition: theme.transitions.create(['margin', 'width'], {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//   },
-//   appBarShift: {
-//     width: `calc(100% - ${drawerWidth}px)`,
-//     marginLeft: drawerWidth,
-//     transition: theme.transitions.create(['margin', 'width'], {
-//       easing: theme.transitions.easing.easeOut,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//   },
-//   hide: {
-//     display: 'none',
-//   },
-//   drawer: {
-//     width: drawerWidth,
-//     flexShrink: 0,
-//   },
-//   drawerPaper: {
-//     width: drawerWidth,
-//   },
-//   drawerHeader: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     padding: theme.spacing(0, 1),
-//     // necessary for content to be below app bar
-//     ...theme.mixins.toolbar,
-//     justifyContent: 'flex-end',
-//   },
-//   content: {
-//     flexGrow: 1,
-//     height: 'calc(100vh - 64px)',
-//     padding: theme.spacing(3),
-//     transition: theme.transitions.create('margin', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//     marginLeft: -drawerWidth,
-//   },
-//   contentShift: {
-//     transition: theme.transitions.create('margin', {
-//       easing: theme.transitions.easing.easeOut,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//     marginLeft: 0,
-//   },
-// }));
-
-// export default function PersistentDrawerLeft() {
-//   const classes = useStyles();
-//   const [open, setOpen] = React.useState(false);
-
-//   const handleDrawerOpen = () => {
-//     setOpen(true);
-//   };
-
-//   const handleDrawerClose = () => {
-//     setOpen(false);
-//   };
-
-//   return (
-//     <div className={classes.root}>
-//       <CssBaseline />
-//       <AppBar
-//         position="fixed"
-//         className={clsx(classes.appBar, {
-//           [classes.appBarShift]: open,
-//         })}
-//       >
-//         <Toolbar>
-//           <IconButton
-//             color="inherit"
-//             aria-label="open drawer"
-//             onClick={handleDrawerOpen}
-//             edge="start"
-//             className={clsx(classes.menuButton, open && classes.hide)}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography variant="h6" noWrap>
-//             Persistent drawer
-//           </Typography>
-//         </Toolbar>
-//       </AppBar>
-//       <Drawer
-//         className={classes.drawer}
-//         variant="persistent"
-//         anchor="left"
-//         open={open}
-//         classes={{
-//           paper: classes.drawerPaper,
-//         }}
-//       >
-//         <div className={classes.drawerHeader}>
-//           <IconButton onClick={handleDrawerClose}>
-//             <ChevronLeftIcon />
-//           </IconButton>
-//         </div>
-//         <Divider />
-//         <Typography variante='h4'>Design Your Palette</Typography>
-//         <NewColorForm />
-//       </Drawer>
-//       <main
-//         className={clsx(classes.content, {
-//           [classes.contentShift]: open,
-//         })}
-//       >
-//         <div className={classes.drawerHeader} />
-//         <h1>HOLAAAA</h1>
-//       </main>
-//     </div>
-//   );
-// }
