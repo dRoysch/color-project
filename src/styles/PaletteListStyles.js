3d4babed813397b25ef320ad936976e1272ce32b
David Arroyo <arroyoschneiderman@gmail.com>
2020-06-15 08:09:04 -0400

    Add bg svg to PalletList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,10 +1,17 @@
+import sizes from './sizes'
+import bg from './bg.svg';
+
 export default {
     root: {
-        backgroundColor: 'blue',
         height: '100vh',
         display: 'flex',
         alignItems: 'flex-start',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        /* background by SVGBackgrounds.com */
+        backgroundColor: '#2818a0',
+        backgroundImage: `url(${bg})`,
+        overflow: 'scroll'
+
     },
     container: {
         width: '50%',
@@ -12,8 +19,14 @@ export default {
         alignItems: 'flex-start',
         flexDirection: 'column',
         flexWrap: 'wrap',
-        // border: '1px solid white'
+        [sizes.down('xl')]: {
+            width: '80%'
+        },
+        [sizes.down('xs')]: {
+            width: '70%'
+        }
     },
+
     nav: {
         display: 'flex',
         width: '100%',
@@ -29,7 +42,17 @@ export default {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%'
+        gridGap: '2.5rem',
+        [sizes.down('md')]: {
+            gridTemplateColumns: 'repeat(2, 50%)'
+        },
+        [sizes.down('xs')]: {
+            gridTemplateColumns: 'repeat(1, 100%)',
+            gridGap: '1.5rem',
 
+        }
+    },
+    heading: {
+        fontSize: '2rem'
     }
 }
