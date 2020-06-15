fd6f2f3a1afeef4341c0298a1716f1dd72adcd8c
David Arroyo <arroyoschneiderman@gmail.com>
2020-06-15 09:34:23 -0400

    Add Delete Confirmation Dialog

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -5,13 +5,48 @@ import styles from './styles/PaletteListStyles.js';
 import { Link } from 'react-router-dom';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
 
-class PaletteList extends Component {
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
 
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
+
+
+class PaletteList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            openDeleteDialog: false,
+            deletingId: ''
+        };
+        this.openDialog = this.openDialog.bind(this);
+        this.closeDialog = this.closeDialog.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    openDialog(id){
+        this.setState({openDeleteDialog: true, deletingId: id})
+    }
+    closeDialog(){
+        this.setState({openDeleteDialog: false, deletingId: ''})
+    }
+    handleDelete(){
+        this.props.delePalette(this.state.deletingId);
+        this.closeDialog();
+    }
     goToPalette(id){
         this.props.history.push('/palette/' + id);
     }
+
     render() {
-        const {palettes, classes, deletePalette} = this.props;
+        const {palettes, classes} = this.props;
+        const {openDeleteDialog} = this.state;
         return (
             <div className={classes.root}>
                 <div className={classes.container}>
@@ -30,7 +65,8 @@ class PaletteList extends Component {
                                     <MiniPalette 
                                     {...palette} 
                                     handleClick={() => this.goToPalette(palette.id)}
-                                    handleDelete={deletePalette}
+                                    // handleDelete={deletePalette}
+                                    openDialog={this.openDialog}
                                     key={palette.id}
                                     id={palette.id}
                                     />
@@ -39,6 +75,33 @@ class PaletteList extends Component {
                         }
                         </TransitionGroup>
                 </div>
+                <Dialog 
+                open={openDeleteDialog} 
+                aria-labelledby='delete-dialog-title' 
+                onClose={this.closeDialog}>
+                    <DialogTitle id='delete-dialog-title'>
+                        Delete this palette?  
+                    </DialogTitle>
+                    <List>
+                        <ListItem button onClick={this.handleDelete}>
+                            <ListItemAvatar>
+                                <Avatar style={{ backgroundColor: blue[100], color: blue[600]}}>
+                                    <CheckIcon />
+                                </Avatar>
+                            </ListItemAvatar>  
+                            <ListItemText primary='Delete'></ListItemText> 
+                        </ListItem>
+
+                        <ListItem button onClick={this.closeDialog}>
+                            <ListItemAvatar>
+                                <Avatar style={{ backgroundColor: red[100], color: red[600]}}>
+                                    <CloseIcon />
+                                </Avatar>
+                            </ListItemAvatar>  
+                            <ListItemText primary='Cancel'></ListItemText> 
+                        </ListItem>
+                    </List>
+                </Dialog>
             </div>
         )
     }
